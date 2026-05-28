f84d8a5a0e11ae0d7173a10aec155f7c64d20fee
Francis_ldn <explorer18@protonmail.com>
2026-05-28 17:10:43 +0100

    update date filter

--- src/components/universe-definition/DateRangeFilter.tsx
+++ src/components/universe-definition/DateRangeFilter.tsx
@@ -18,7 +18,7 @@ type DateRange = {
 export function DateRangeFilter() {
   const { setFilterDateRange } = useUniverseStore()
   const [range, setRange] = React.useState<DateRange>({
-    from: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), // Default to one week range
+    from: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // Default to one week range
     to: new Date(Date.now()), // Default to one week range
   })
 
